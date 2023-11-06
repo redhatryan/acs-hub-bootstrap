5ba5490effd9eec5988e84a92c10ef277e546bc0
Ryan Etten <retten@redhat.com>
2023-11-06 13:57:50 -0600

    add dns to script

--- bootstrap-acs-local.sh
+++ bootstrap-acs-local.sh
@@ -5,6 +5,10 @@ SLEEP_SECONDS=30
 
 echo ""
 
+echo "Update DNS (permanent solution coming soon)"
+
+kustomize build github.com/redhatryan/cluster-config/components/dns | oc apply -f -
+
 echo "Installing secrets on local ACS hub to bootstrap ACS with"
 
 kustomize build bootstrap/secrets/base | oc apply -f -
